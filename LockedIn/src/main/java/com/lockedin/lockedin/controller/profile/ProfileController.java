568d15efcfb539a37d2883455e356793b6bc5cc6
Ak-Pras <akhilesh.prasad@connect.qut.edu.au>
2026-05-01 09:59:00 +1000

    Initial implement of Activity Summary in Profile Page

--- LockedIn/src/main/java/com/lockedin/lockedin/controller/profile/ProfileController.java
+++ LockedIn/src/main/java/com/lockedin/lockedin/controller/profile/ProfileController.java
@@ -1,7 +1,10 @@
 package com.lockedin.lockedin.controller.profile;
 
 import java.time.LocalDate;
+import java.util.Date;
 
+import com.lockedin.lockedin.model.dao.FoodDAO;
+import com.lockedin.lockedin.model.dao.WorkoutRoutineDAO;
 import com.lockedin.lockedin.model.entity.User;
 import com.lockedin.lockedin.model.session.CurrentUser;
 import javafx.fxml.FXML;
@@ -20,7 +23,19 @@ public class ProfileController {
     @FXML
     private Label fitnessGoalLabel;
     @FXML
+    private Label totalWorkoutsLabel;
+    @FXML
+    private Label avgCalLabel;
+    @FXML
+
+    private WorkoutRoutineDAO routineDAO;
+    private FoodDAO foodDAO;
+
+    @FXML
     private void initialize() {
+        routineDAO = new WorkoutRoutineDAO();
+        foodDAO = new FoodDAO();
+
         user = CurrentUser.get();
         int age = LocalDate.now().getYear() - user.getDateOfBirth().getYear();
         ageLabel.setText("Age: " + age);
@@ -28,5 +43,8 @@ public class ProfileController {
         weightLabel.setText("Weight: " + user.getWeight() + " kg");
         fitnessGoalLabel.setText("Fitness Goal: " + user.getFitnessGoal());
         nameLabel.setText("Hello " + user.getFirstName() + "!");
+        totalWorkoutsLabel.setText("Total Workouts: " + routineDAO.getTotalWorkoutsByUser(user.getId()));
+        // Need to update to actually get average over the previous seven days, when currently only gets today's total calories
+        avgCalLabel.setText("Avg Calories: " + foodDAO.getDailyTotalByAttribute(new Date(), "calories", user.getId()));
     }
 }
