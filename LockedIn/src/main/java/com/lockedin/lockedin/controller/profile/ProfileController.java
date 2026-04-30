95e547f8662f12e5ab6264c18a9696a5dd5546c5
mimikkyyu <miminguyen322@gmail.com>
2026-05-01 01:24:41 +1000

    feat: add logout button to profile page

--- LockedIn/src/main/java/com/lockedin/lockedin/controller/profile/ProfileController.java
+++ LockedIn/src/main/java/com/lockedin/lockedin/controller/profile/ProfileController.java
@@ -1,16 +1,23 @@
 package com.lockedin.lockedin.controller.profile;
 
+import com.lockedin.lockedin.controller.auth.Authentication;
 import com.lockedin.lockedin.model.entity.User;
 import com.lockedin.lockedin.model.session.CurrentUser;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 /**
  * Controller for the Profile page.
  * Displays the currently logged-in user's personal information.
  */
 public class ProfileController {
+    private static final String LOGIN_VIEW = "/com/lockedin/lockedin/pages/auth/login-view.fxml";
+
     private User user;
+    @FXML private Button logoutBtn;
     @FXML
     private Label nameLabel;
     @FXML
@@ -21,6 +28,12 @@ public class ProfileController {
     private Label weightLabel;
     @FXML
     private Label fitnessGoalLabel;
+    @FXML
+    private void handleLogout() throws IOException {
+        CurrentUser.clear();
+        Authentication.switchScene(logoutBtn, LOGIN_VIEW);
+    }
+
     @FXML
     private void initialize() {
         user = CurrentUser.get();
