a4b159628b33e73b4fd0b2ed9c1dd58f044c091a
mimikkyyu <miminguyen322@gmail.com>
2026-05-01 00:49:08 +1000

    fix: remove duplicate getAge() method, keep correct ChronoUnit implementation

--- LockedIn/src/main/java/com/lockedin/lockedin/model/entity/User.java
+++ LockedIn/src/main/java/com/lockedin/lockedin/model/entity/User.java
@@ -87,10 +87,6 @@ public class User {
         return dateOfBirth;
     }
 
-    public int getAge() {
-        return LocalDate.now().getYear() - dateOfBirth.getYear();
-    }
-
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
