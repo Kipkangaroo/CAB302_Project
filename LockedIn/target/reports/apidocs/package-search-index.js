eb77a66273fd6955758e735830e96c471b88be11
Tusar Paudel <n12834041@qut.edu.au>
2026-05-20 13:23:32 +1000

    Publish JavaDoc to GitHub Pages via Actions workflow

---
+++ LockedIn/target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.lockedin.lockedin","l":"com.lockedin.lockedin.app"}];updateSearchResults();
---
+++ LockedIn/target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.lockedin.lockedin.app","l":"JohnDemoSeeder"},{"p":"com.lockedin.lockedin.app","l":"LockedIn"}];updateSearchResults();
